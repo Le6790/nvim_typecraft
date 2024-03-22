12b553604edbe4de3ed0d45077f181a7adb01b1d
Kent Le <le6790@gmail.com>
2024-03-21 18:32:12 -0600

    Fixed tab selection in autocomplete window

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -16,12 +16,15 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip") -- For `luasnip` users.
       require("luasnip.loaders.from_vscode").lazy_load()
+
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
+
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -32,7 +35,8 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
+        --mapping = cmp.mapping.preset.insert({
+        mapping = ({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -43,7 +47,7 @@ return {
               cmp.select_next_item()
               -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
               -- that way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
+            elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
             elseif has_words_before() then
               cmp.complete()
