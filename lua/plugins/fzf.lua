2735e802902dd4f04a5dfca028f0ceb0bd28bbfa
Kent Le <le6790@gmail.com>
2024-12-17 23:28:04 -0700

    formatting

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,15 +1,14 @@
 return {
-  "junegunn/fzf.vim",
-  commit="e69f2dcdad44e7eafe764969ed7281d7290db18f",
-  dependencies = {
-    "junegunn/fzf",
-    commit="5e47ab94318a0a43b253bd09b5a18ef61cd7de1b",
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>pf", ":Files<CR>")
-    vim.keymap.set("n", "<leader>pr", ":History<CR>")
-    vim.keymap.set("n", "<leader>pt", ":Rg<CR>")
-    vim.keymap.set("n", "<leader>pb", ":Buffers<CR>")
-
-  end,
+	"junegunn/fzf.vim",
+	commit = "e69f2dcdad44e7eafe764969ed7281d7290db18f",
+	dependencies = {
+		"junegunn/fzf",
+		commit = "5e47ab94318a0a43b253bd09b5a18ef61cd7de1b",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>pf", ":Files<CR>")
+		vim.keymap.set("n", "<leader>pr", ":History<CR>")
+		vim.keymap.set("n", "<leader>pt", ":Rg<CR>")
+		vim.keymap.set("n", "<leader>pb", ":Buffers<CR>")
+	end,
 }
