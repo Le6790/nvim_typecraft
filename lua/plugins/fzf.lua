dffdc6fd2a920fbe33d7dd7913cf719a4c47c7ea
Kent Le <le6790@gmail.com>
2026-03-10 00:35:02 -0600

    reformatted

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,20 +1,20 @@
 return {
-	"ibhagwan/fzf-lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("fzf-lua").setup({
-			winopts = {
-				preview = {
-					layout = "horizontal",
-					horizontal = "right:50%",
-				},
-			},
-			previewers = {
-				builtin = {
-					render_markdown = { enabled = false },
-				},
-			},
-		})
-	end,
-	-- Keymaps in vim-keymaps.lua
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("fzf-lua").setup({
+      winopts = {
+        preview = {
+          layout = "horizontal",
+          horizontal = "right:50%",
+        },
+      },
+      previewers = {
+        builtin = {
+          render_markdown = { enabled = false },
+        },
+      },
+    })
+  end,
+  -- Keymaps in vim-keymaps.lua
 }
